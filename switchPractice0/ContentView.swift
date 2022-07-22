7ae3062f1565585983cbdad0cbe15abf31e4b821
Wongi <hpurs95@gmail.com>
2022-07-22 10:14:37 +0900

    Initial Commit

---
+++ switchPractice0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  switchPractice0
+//
+//  Created by MacBook Air on 2022/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
